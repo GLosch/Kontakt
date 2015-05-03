64557203dd6af8ccbdb384cf88583bd118a333eb
Gabby Losch <glosch00@gmail.com>
2015-05-03 16:25:56 -0400

    adding new categories working, and page automatically refreshes

--- public/main.js
+++ public/main.js
@@ -1,24 +1,23 @@
 $(document).ready(function(){
   console.log("Linked");
 
+  var $container = $('.ui.categories-container');
   var colors = ['blue', 'green', 'red', 'orange', 'purple', 'teal'];
   //initial ajax call to database to retrieve categories
-  $.ajax({
+  function refreshCategories(){
+    $container.empty();
+    $.ajax({
       url: '../categories',
       method: 'GET',
       contentType: 'application/JSON'
     }).done(function(catData){
-      console.log(catData);
       //nested ajax call to database to retrieve individual contact info
       $.ajax({
         url: '../contacts',
         method: 'GET',
         contentType: 'application/JSON'
       }).done(function(contactData){
-        console.log(contactData);
           var totalContacts = contactData.length;
-          console.log(totalContacts);
-          var $container = $('.ui.categories-container');
           var $allContactsBlock = "<div class='ui blue inverted piled segment'><h4 class='ui header'>All Contacts</h4><h5 class='ui header'>" + totalContacts + " Contacts</h5></div>";
           $container.append($allContactsBlock);
           var i = 1;
@@ -33,16 +32,46 @@ $(document).ready(function(){
         });
       });
     });
+  }
+
+  refreshCategories();
 
   var $newCatButton = $(".ui.fluid.three.item.menu").children(".new-cat").first();
   var $allCatsButton = $(".ui.fluid.three.item.menu").children(".all-cats").first();
+
+  function defaultActives(){
+    $newCatButton.removeClass("blue active");
+    $allCatsButton.addClass("blue active");
+  }
 
   $newCatButton.on("click", function(){
     console.log("clicked!");
     $('.ui.modal').modal('show');
     $newCatButton.addClass("blue active");
     $allCatsButton.removeClass("blue active");
+    $('.ui.modal').on("click", "[data-action='save']", function(event){
+      console.log("clicked save");
+      var $newCatInput = $('.ui.modal').find("input").val();
+      $.ajax({
+        url: '../categories',
+        method: 'POST',
+        contentType: 'application/JSON',
+        data: JSON.stringify({name: $newCatInput})
+      }).done(function(){
+        refreshCategories();
+      });
+      defaultActives();
+    });
+    $('.ui.modal').on("click", "[data-action='cancel']", function(event){
+      defaultActives();
+    });
+
+    // $('.ui.modal').off('click', function(){
+    //   defaultActives();
+    // });
   });
+
+  // $newCatButton.off("click", $(".ui.modal"), defaultActives());
   
 
 
