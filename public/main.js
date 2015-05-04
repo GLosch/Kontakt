cd4bc6ccb70e0dbfdfea4561f9435194fbb48eef
Gabby Losch <glosch00@gmail.com>
2015-05-04 17:07:56 -0400

    updating a contact's info now working

--- public/main.js
+++ public/main.js
@@ -1,46 +1,79 @@
-$(document).ready(function(){
-  console.log("Linked");
+//sets the homepage path to '...#/' so that the refreshCategories function can run on initial page load
+document.location.href = '#/';
 
-  //initial ajax call to database to retrieve categories
-  function refreshCategories(){
-    var $container = $('.ui.categories-container');
-    var colors = ['blue', 'green', 'red', 'orange', 'purple', 'teal'];
-    $container.empty();
+var $container = $('.ui.categories-container');
+var $allCatsButton = $(".ui.fluid.three.item.menu").children(".all-cats").first();
+var $aboutLink = $(".ui.fluid.three.item.menu").children(".about").last();
+
+//Mustache template for Cards
+var $personCard = "<div class='ui cards'>{{#person}}<div data-id='{{id}}' class='card'><a class='image'><img src='{{image}}'></a><div class='content'><p data-type='name' class='header' contenteditable='true'>{{name}}</p><div class='description'><p data-type='city' contenteditable='true'>{{city}}</p><p data-type='phone' contenteditable='true'>{{phone}}</p><p data-type='email' contenteditable='true'>{{email}}</p></div></div></div>{{/person}}</div>";
+
+//get all Contacts
+function allContacts(){
+  $.ajax({
+    url: '../contacts',
+    method: 'GET',
+    contentType: 'application/JSON'
+  }).done(function(data){
+    console.log(data);
+    // $container.empty();
+    // data.forEach(function(e){
+    //   var image = e.image_url;
+    //   var name = e.name;
+    //   var city = e.city;
+    //   var phone = e.phone;
+    //   var email = e.email;
+    //   var personInfo = {image: image, name: name, city: city, phone: phone, email: email};
+    //   var $rendered = Mustache.render($personCard, {person: personInfo});
+    //   $container.append($rendered);
+    // });
+  });
+}
+
+
+//initial ajax call to database to retrieve categories
+function refreshCategories(){
+  var colors = ['blue', 'green', 'red', 'orange', 'purple', 'teal'];
+  $container.empty();
+  $allCatsButton.addClass("blue active");
+  $aboutLink.removeClass("blue active");
+  $.ajax({
+    url: '../categories',
+    method: 'GET',
+    contentType: 'application/JSON'
+  }).done(function(catData){
+    //nested ajax call to database to retrieve individual contact info
     $.ajax({
-      url: '../categories',
+      url: '../contacts',
       method: 'GET',
       contentType: 'application/JSON'
-    }).done(function(catData){
-      //nested ajax call to database to retrieve individual contact info
-      $.ajax({
-        url: '../contacts',
-        method: 'GET',
-        contentType: 'application/JSON'
-      }).done(function(contactData){
-          var totalContacts = contactData.length;
-          var $allContactsBlock = "<a class='category-block' href='#'><div class='ui blue inverted piled segment'><h4 class='ui header'>All Contacts</h4><h5 class='ui header'>" + totalContacts + " Contacts</h5></div></a>";
-          $container.append($allContactsBlock);
-          var i = 1;
-          catData.forEach(function(e){
-            var $catBlock = "<a data-id='" + e.id + "' class='category-block' href='#/categories/" + e.name + "'><div data-id='" + e.id + "' class='ui " + colors[i] + " inverted piled segment'><h4 data-id='" + e.id + "' class='ui header'>" + e.name + "</h4><h5 data-id='" + e.id + "' class='ui header'>" + "" + " Contacts</h5></div></a>";
-            $container.append($catBlock);
-            //increment through list of colors and assign the next color in the sequence to each category block
-            if (i === 5){
-              i = 0;
-            } else {
-              i++;
-            }
-        });
+    }).done(function(contactData){
+        var totalContacts = contactData.length;
+        var $allContactsBlock = "<a class='category-block' href='#/all'><div class='ui blue inverted piled segment'><h4 class='ui header'>All Contacts</h4><h5 class='ui header'>" + totalContacts + " Contacts</h5></div></a>";
+        $container.append($allContactsBlock);
+        var i = 1;
+        catData.forEach(function(e){
+          var $catBlock = "<a data-id='" + e.id + "' class='category-block' href='#/categories/" + e.name + "'><div data-id='" + e.id + "' class='ui " + colors[i] + " inverted piled segment'><h4 data-id='" + e.id + "' class='ui header'>" + e.name + "</h4><h5 data-id='" + e.id + "' class='ui header'>" + "" + " Contacts</h5></div></a>";
+          $container.append($catBlock);
+          //increment through list of colors and assign the next color in the sequence to each category block
+          if (i === 5){
+            i = 0;
+          } else {
+            i++;
+          }
       });
     });
-  }
+  });
+}
+
+$(document).ready(function(){
+  console.log("Linked");
 
   if(window.location.href === 'http://localhost:3000/'){
     refreshCategories();
   }
 
   var $newCatButton = $(".ui.fluid.three.item.menu").children(".new-cat").first();
-  var $allCatsButton = $(".ui.fluid.three.item.menu").children(".all-cats").first();
 
   //assign the active class to the Categories button and remove it from the All Categories button
   function defaultActives(){
@@ -70,16 +103,96 @@ $(document).ready(function(){
     $('.ui.modal').on("click", "[data-action='cancel']", function(event){
       defaultActives();
     });
-
-    // $('.ui.modal').off('click', function(){
-    //   defaultActives();
-    // });
   });
 
-  // $newCatButton.off("click", $(".ui.modal"), defaultActives());
-  
   //end modal//
 
+  //expand category
+  $('.ui.categories-container').on("click", "a.category-block", function(event){
+    var $targetID = ($(this).data().id);
+    $container.empty();
+    $.ajax({
+      url: '../contacts',
+      method: 'GET',
+      contentType: 'application/JSON'
+    }).done(function(data){
+      var categoryResults = _.where(data, {category_id: $targetID});
+      if(categoryResults.length === 0){
+        $container.append("<div class='ui segment'><p>There are no contacts in this category</p></div>");
+      } else{
+        categoryResults.forEach(function(e){
+          var id = e.id;
+          var image = e.image_url;
+          var name = e.name;
+          var city = e.city;
+          var phone = e.phone;
+          var email = e.email;
+          var personInfo = {id: id, image: image, name: name, city: city, phone: phone, email: email};
+          var $rendered = Mustache.render($personCard, {person: personInfo});
+          $container.append($rendered);
+        });
+      }
+    });
+  });
+
+  //update contact info (from within a person Card)
+  $('.ui.categories-container').on("blur", "[contenteditable='true']", function(event){
+    var clickedCard = $(event.target).parents('.card');
+    var $typeOfData = ($(event.target).data().type);
+    var valueOfField = $(event.target)[0].innerText.trim();
+    var cardID = clickedCard.data().id;
+    //switch case to determine which field has been edited, and make an ajax call to edit only that field
+    switch($typeOfData){
+      case 'name':
+        $.ajax({
+          url: '../contacts/' + cardID,
+          method: 'PATCH',
+          contentType: 'application/JSON',
+          data: JSON.stringify({name: valueOfField})
+        }).done(function(){
+          console.log("updated");
+        });
+      break;
+
+      case 'city':
+        $.ajax({
+          url: '../contacts/' + cardID,
+          method: 'PATCH',
+          contentType: 'application/JSON',
+          data: JSON.stringify({city: valueOfField})
+        }).done(function(){
+          console.log("updated");
+        });
+      break;
+
+      case 'phone':
+        $.ajax({
+          url: '../contacts/' + cardID,
+          method: 'PATCH',
+          contentType: 'application/JSON',
+          data: JSON.stringify({phone: valueOfField})
+        }).done(function(){
+          console.log("updated");
+        });
+      break;
+
+      case 'email':
+        $.ajax({
+          url: '../contacts/' + cardID,
+          method: 'PATCH',
+          contentType: 'application/JSON',
+          data: JSON.stringify({email: valueOfField})
+        }).done(function(){
+          console.log("updated");
+        });
+      break;
+    }
+  });
+
+});
+
+
+//fade effects that need adjustment
   // $(".ui.categories-container").on("mouseenter", ".category-block", function(){
   //   console.log("mouse enter");
   //   $(this).css("opacity", 0.5);
@@ -98,18 +211,3 @@ $(document).ready(function(){
   //  function(){
   //     $(this).stop().fadeTo('fast', 0.8);
   //  });
-
-  //expand category
-  $('.ui.categories-container').on("click", "a.category-block", function(event){
-    var $targetID = ($(this).data().id);
-    console.log($targetID);
-    // $.ajax({
-    //   url: '../'
-    // })
-  });
-
-
-  var $aboutLink = $(".ui.fluid.three.item.menu").children(".about").last();
-  
-
-});
--- public/router.js
+++ public/router.js
@@ -5,40 +5,40 @@ var $container = $(".ui.categories-container");
 
 var $aboutHTML = "<div class='ui segment'><p>This site was designed and built by Gabby Losch using JQuery, Semantic UI, and various JavaScript libraries and modules.</p></div>";
 
-var $personCard = "<div class='ui cards'>{{#person}}<div class='card'><a class='image'><img src='{{image}}'></a><div class='content'><a class='header'>{{name}}</a><div class='meta'>        <a>Category</a></div><div class='description'>{{city}}<br>{{phone}}<br>{{email}}</div></div></div>{{/person}}</div>";
-
 function aboutPage(){
   $container.empty();
   var $aboutLink = $(".ui.fluid.three.item.menu").children(".about").last();
   console.log($aboutLink);
-  // $allCatsButton.removeClass("blue active");
-  // $aboutLink.addClass("blue active");
+  $allCatsButton.removeClass("blue active");
+  $aboutLink.addClass("blue active");
   $container.append($aboutHTML);
 }
 
-function categoryPage(){
-  $container.empty();
-    $.ajax({
-      url: '../contacts',
-      method: 'GET',
-      contentType: 'application/JSON'
-    }).done(function(data){
-      data.forEach(function(e){
-        var image = e.image_url;
-        var name = e.name;
-        var city = e.city;
-        var phone = e.phone;
-        var email = e.email;
-        var personInfo = {image: image, name: name, city: city, phone: phone, email: email};
-        var $rendered = Mustache.render($personCard, {person: personInfo});
-        $container.append($rendered);
-      });
-    });
-}
+// function categoryPage(catName){
+//   $container.empty();
+//     $.ajax({
+//       url: '../contacts',
+//       method: 'GET',
+//       contentType: 'application/JSON'
+//     }).done(function(data){
+//       data.forEach(function(e){
+//         var image = e.image_url;
+//         var name = e.name;
+//         var city = e.city;
+//         var phone = e.phone;
+//         var email = e.email;
+//         var personInfo = {image: image, name: name, city: city, phone: phone, email: email};
+//         var $rendered = Mustache.render($personCard, {person: personInfo});
+//         $container.append($rendered);
+//       });
+//     });
+// }
 
 var routes = {
+  '/': refreshCategories,
   '/about': aboutPage,
-  '/categories': categoryPage
+  '/categories': refreshCategories
+  // '/all': allContacts
 };
 
 var router = Router(routes);
